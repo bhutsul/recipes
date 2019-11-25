9394f85d88c1984dcff5f3e1d91e2be34d6f9c05
Bogdan <bogdanhgf56gmail.com>
2019-11-25 12:30:44 +0200

    remove space and methods

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
 use App\Ingredient;
 use App\Recipe;
 use Illuminate\Http\Request;
@@ -61,7 +60,6 @@ class SearchController extends Controller
             }])->whereIn('id', $arrayIngredientsId)->get();
 
             foreach ($recipesAndIngredients as $recipes) {
-
                 foreach ($recipes['recipes'] as $infoRecipes) {
                     unset($infoRecipes['pivot']);
                     $data[] = $infoRecipes;
@@ -69,71 +67,5 @@ class SearchController extends Controller
             }
             return response()->json($data)->setEncodingOptions(JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
         }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Search  $search
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sarch $search)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Search  $search
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Search $search)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Search  $search
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Search $search)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Search  $search
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Search $search)
-    {
-        //
     }
 }
