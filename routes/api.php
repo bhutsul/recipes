eac34c91b4ec73776f810b968ad5c29c74ad710b
bhutsul <bogdanhgf56@gmail.com>
2019-12-08 19:41:25 +0200

    added corse and group route

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware' => 'cors'], function () {
+    Route::get('recipes','RecipeController@show');
+    Route::get('categories','CategoryController@show')->middleware('cors');
 
-Route::get('recipes','RecipeController@show');
-Route::get('categories','CategoryController@show');
+    Route::get('search','SearchController@index');
+    Route::post('add','AddRecipeController@create');
 
-Route::get('search','SearchController@index');
-Route::post('add','AddRecipeController@create');
+    Route::post('registration','Auth\RegisterController@registration');
+    Route::post('login','Auth\LoginController@login');
 
-Route::post('registration','Auth\RegisterController@registration');
-Route::post('login','Auth\LoginController@login');
-Route::post('logout', 'Auth\LogoutController')->middleware('auth:api');
-
-Route::group(['middleware' => ['web']], function () {
-    Route::get('social-auth/google', 'Auth\LoginController@redirectToProviderLogin');
-    Route::get('social-auth/google/callback', 'Auth\LoginController@authenticateSocial');
+    Route::group(['middleware' => ['web']], function () {
+        Route::get('social-auth/google', 'Auth\LoginController@redirectToProviderLogin');
+        Route::get('social-auth/google/callback', 'Auth\LoginController@authenticateSocial');
+    });
 });
